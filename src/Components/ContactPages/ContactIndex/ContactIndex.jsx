b25222b09dc82d64921336af7ba8f498cd76af3d
Eddington <eampofu@gmail.com>
2024-04-16 20:14:28 +0200

    Add Contact in Action

--- src/Components/ContactPages/ContactIndex/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex/ContactIndex.jsx
@@ -51,7 +51,15 @@ class ContactIndex extends React.Component {
 		};
 	}
 
-	handleAddContact = () => {
+	handleAddContact = (newContact) => {
+		const newFinalContact = {
+			...newContact,
+			id: this.state.contactList[this.state.contactList.length - 1].id + 1,
+			isFavourite: false,
+		};
+		this.setState((prevState) => {
+			return { contactList: prevState.contactList.concat([newFinalContact]) };
+		});
 		alert("h");
 	};
 
