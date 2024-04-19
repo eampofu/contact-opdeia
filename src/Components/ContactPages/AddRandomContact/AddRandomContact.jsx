b311e146ed0c174eadee41828db9479584d12c12
Eddington <eampofu@gmail.com>
2024-04-19 20:58:30 +0200

    making the axios call

--- src/Components/ContactPages/AddRandomContact/AddRandomContact.jsx
+++ src/Components/ContactPages/AddRandomContact/AddRandomContact.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { getRandomUser } from "../../../utility/api";
+
+const GetRandomContact = async () => {
+	const responseFromAPI = await getRandomUser();
+	console.log(responseFromAPI);
+};
 
 const AddRandomContact = () => {
 	return (
 		<div>
-			<button className="btn btn-success form-control">
+			<button
+				className="btn btn-success form-control"
+				onClick={() => GetRandomContact()}
+			>
 				Add Random Contact
 			</button>
 		</div>
